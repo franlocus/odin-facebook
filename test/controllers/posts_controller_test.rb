3173932bac3e5a5c67d70cacab8a373e06874384
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-11 00:25:56 -0300

    add devise test helper and tests for root path logged and not logged

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get posts_index_url
-    assert_response :success
+  setup do
+    sign_in users(:one)
+  end
+
+  test "redirected if not logged in" do
+    sign_out :user
+
+    get root_path
+    assert_response :redirect
+  end
+
+  test "login user should get Posts#index" do
+    get root_path
+    assert_select 'h1', 'Posts#index'
   end
 end

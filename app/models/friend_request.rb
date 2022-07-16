361ab81ead33cf67c472f6284c8a6e09bcbe601a
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-16 00:25:42 -0300

    restore correct association user with friend request

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,6 +1,6 @@
 class FriendRequest < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
+  belongs_to :sended_request, class_name: 'User', foreign_key: :user_id
+  belongs_to :received_request, class_name: 'User', foreign_key: :friend_id
 
   validates_uniqueness_of :user_id, scope: :friend_id
 end

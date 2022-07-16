0395f8749ca0bdc7fe98571ff1ea0f377e7dafc2
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-16 00:47:14 -0300

    refactor user dependents and add tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not have sended requests until the user create request" do
+   assert_empty users(:one).sended_requests
+   users(:one).friend_requests.build(friend_id: users(:two).id).save
+
+   assert_not_empty users(:one).sended_requests
+ end
+
+  test "should not have request if user is destroyed" do
+   FriendRequest.create(user_id: users(:one).id, friend_id: users(:two).id)
+   users(:one).destroy
+
+   assert_empty users(:two).received_requests
+ end
 end

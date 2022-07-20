f687b4944611e9a217a4efb5d34134abf479469a
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-19 22:33:26 -0300

    friend requests now have inverses

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -4,7 +4,14 @@ class FriendRequest < ApplicationRecord
 
   validates_uniqueness_of :user_id, scope: :friend_id
 
-  def self.user_associated_requests(current_user)
-    where('user_id = :current_user_id OR friend_id = :current_user_id', { current_user_id: current_user.id })
+  after_create :create_inverse
+  before_destroy :destroy_inverse
+
+  def create_inverse
+    FriendRequest.create(user_id: friend_id, friend_id: user_id, status: 'pending', inverse: true)
+  end
+
+  def destroy_inverse
+    FriendRequest.find_by_user_id_and_friend_id(friend_id, user_id).delete
   end
 end

5ea62d93a42afdbb0267b215114959927bd322ba
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-21 22:29:35 -0300

    add friendship tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -4,11 +4,18 @@ class FriendshipTest < ActiveSupport::TestCase
   test "create friendship if doesnt exit and reject if already exist" do
     user_id = friendships(:one).user_id
     friend_id = friendships(:one).friend_id
-    Friendship.destroy_all
+    Friendship.delete_all
 
+    # create friendship
     friendship = Friendship.new(user_id: user_id, friend_id: friend_id)
     assert friendship.save
 
+    # check inverse creation
+    assert_not friendship.inverse?
+    friendship_inverse = Friendship.find_by_user_id_and_friend_id(friend_id, user_id) 
+    assert friendship_inverse.inverse?
+
+    # check failing validation of uniqueness 
     already_friendship = Friendship.new(user_id: user_id, friend_id: friend_id)
 
     assert_not already_friendship.save

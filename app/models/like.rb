c5d25d4c64f4e7e2470181ea17a892fc93d89e06
franlocus <78037877+franlocus@users.noreply.github.com>
2022-07-30 21:50:26 -0300

    Add counter caching likes in Post; show the count in view

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, counter_cache: true
 
   validates_uniqueness_of :user_id, scope: :post_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_013619) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_002442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_013619) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

3123a955e1c2783364c84774e9f7014691010da3
franlocus <78037877+franlocus@users.noreply.github.com>
2022-08-07 20:09:00 -0300

    remake friendship tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -2,28 +2,27 @@ require "test_helper"
 
 class FriendshipTest < ActiveSupport::TestCase
   test "create friendship if doesnt exit and reject if already exist" do
-    user_id = friendships(:one).user_id
-    friend_id = friendships(:one).friend_id
-    Friendship.delete_all
-
     # create friendship
-    friendship = Friendship.new(user_id: user_id, friend_id: friend_id)
+    friendship = Friendship.new(user_id: users.first.id, friend_id: users.third.id)
     assert friendship.save
 
-    # check inverse creation
-    assert_not friendship.inverse?
-    friendship_inverse = Friendship.find_by_user_id_and_friend_id(friend_id, user_id) 
-    assert friendship_inverse.inverse?
+    # user first and third should not be friends until the friendship is accepted
+    assert_not_includes users.first.friends, users.third
+    assert_not friendship.accepted
+
+    assert_changes 'users.first.friends' do
+      friendship.update_column(:accepted, true)
+    end
 
     # check failing validation of uniqueness 
-    already_friendship = Friendship.new(user_id: user_id, friend_id: friend_id)
-
-    assert_not already_friendship.save
+    assert_raise ActiveRecord::RecordInvalid do
+      Friendship.create!(user_id: users.first.id, friend_id: users.third.id)
+    end
   end
 
-  test "should destroy friendship is user is destroyed" do
-    users(:one).destroy
-
-    assert_empty users(:two).friends
+  test "should destroy friendship is one of the users is destroyed" do
+    assert_changes 'users.second.friends' do
+      users(:first).destroy
+    end
   end
 end

8f9318021b806bde667326691b8d5647b5a67050
franlocus <78037877+franlocus@users.noreply.github.com>
2022-08-09 23:16:47 -0300

    add posts comments controller and views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post), notice: 'Comment created successfully!'
+    else
+      flash[:comment_errors] = @comment.errors.full_messages
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+end

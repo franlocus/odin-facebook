c06ed716dbf62d21441922d3f5e08657873352bf
franlocus <78037877+franlocus@users.noreply.github.com>
2022-08-11 20:16:50 -0300

    add tests for like and notifications

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates uniqueness user id in scope post id' do
+    assert users.second.likes.create(post: posts.first)
+
+    assert_raise ActiveRecord::RecordInvalid do
+      users.second.likes.create!(post: posts.first) 
+    end
+  end
+
+  test "liking a post creates a notification to post author " do
+    assert_changes 'posts.first.author.notifications.size' do
+      users.second.likes.create(post: posts.first)
+    end
+    assert_equal users.second, posts.first.author.notifications.last.actor
+    assert_equal 'second@email.com likes your post: This is post ONE', posts.first.author.notifications.last.body
+  end
 end

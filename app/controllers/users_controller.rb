a45e2dd17e9a253e6a1112fd6b4d08c23844112d
franlocus <78037877+franlocus@users.noreply.github.com>
2022-08-23 22:16:04 -0300

    add users#show contains users's posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @posts = current_user.posts.includes(:author, comments: :commenter).with_attached_images.order(created_at: :DESC)
+    @current_user_likes_posts_ids = current_user.likes.pluck(:id, :post_id)
   end
 end

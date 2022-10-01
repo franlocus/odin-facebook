9137438aceff177eda56053a5ebc7818d69fe09f
franlocus <78037877+franlocus@users.noreply.github.com>
2022-10-01 00:15:13 -0300

    fix users#show querying wrong likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:author, comments: :commenter).with_attached_images.order(created_at: :DESC)
-    @plucked_likes_and_posts_ids_of_user_likes = @user.likes.pluck(:id, :post_id)
+    @plucked_likes_and_posts_ids_of_user_likes = current_user.likes.pluck(:id, :post_id)
   end
 end

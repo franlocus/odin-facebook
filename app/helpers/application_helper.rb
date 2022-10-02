a905e6191b9ee30af659ad75649e74e8a5285e0c
franlocus <78037877+franlocus@users.noreply.github.com>
2022-10-01 22:42:25 -0300

    add active class dinamically based on current page and improve current user log in legend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def active_class?(class_name = nil, path)
+    class_name ||= ""
+    class_name += " active" if current_page?(path)
+    class_name.strip!
+    return class_name
+  end
 end

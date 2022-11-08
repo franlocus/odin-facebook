becfb0c36051ea54b7ed4a1b8abc40da4ab3dbc6
franlocus <franciscobuscema@gmail.com>
2022-11-08 16:41:09 -0300

    add gem shoulda matchers for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,10 @@ RSpec.configure do |config|
   # FactoryBot helpers
   config.include FactoryBot::Syntax::Methods
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end

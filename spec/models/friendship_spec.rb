1f9b4d72ad7315d13b9710ab53da1d388cd0d83a
franlocus <franciscobuscema@gmail.com>
2022-11-09 20:16:45 -0300

    add tests for friendships

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  context 'validations' do
-    it 'must have a user and friend' do
-      expect { create(:friendship, friend: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  subject(:friendship) { create(:friendship) }
+
+  describe 'using standard factory' do
+    it { should be_valid }
+  end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:user_id).scoped_to(:friend_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:friend).class_name('User').with_foreign_key(:friend_id) }
+    it { should have_many(:notifications) }
+  end
+
+  describe 'notifications creation callbacks' do
+    context 'when it is created should also create a new request notification' do
+      it { expect(friendship.notifications.first.body).to match(/New Friend Request/) }
     end
 
-    it 'must be only one friendship between two users' do
-      user = create(:user)
-      friend = create(:user)
-      create(:friendship, user:, friend:)
-      expect { create(:friendship, user:, friend:) }.to raise_error(ActiveRecord::RecordInvalid)
+    context 'when it is created should also create an accepted request notification' do
+      before { friendship.update(accepted: true) }
+
+      it { expect(friendship.notifications.last.body).to match(/accepted your friend request/) }
     end
   end
 end

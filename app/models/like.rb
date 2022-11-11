8e47670d3096d278b8a85e563a146ac724fefda6
franlocus <franciscobuscema@gmail.com>
2022-11-11 10:35:06 -0300

    fix like has one notification (not many)

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
-  has_many :notifications, as: :notifiable
+  has_one :notification, as: :notifiable
 
   validates_uniqueness_of :user_id, scope: :post_id
 
